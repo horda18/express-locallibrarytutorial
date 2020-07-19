072bc89b6118e835480a9adbb1c9e7ec5de8f0b2
Juan <matareyes91@hotmail.com>
2020-07-18 22:51:08 -0300

    update

---
+++ public/javascripts/main.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function(){
+  $('#eliminar').click(function(){
+    const id = $(this).data('id');
+    var datos=$('input#id').val();
+      $.ajax({
+          type:"POST",
+          url:id+'/delete',
+          data:datos,
+      })
+  });
+});
+*/
